d8c942bce13a7c8b52e59dd978a280dc9ee638c7
guigon95 <guilherme_gon@live.com>
2025-03-26 22:17:32 -0300

    initial commit

---
+++ src/main/kotlin/com/guigon/api_cartoes/ApiCartoesApplication.kt
@@ -0,0 +1,11 @@
+package com.guigon.api_cartoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiCartoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiCartoesApplication>(*args)
+}
