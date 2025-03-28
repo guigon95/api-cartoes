2c20346305276a1605f1212db20d77e2b43d0ea0
guigon95 <guilherme_gon@live.com>
2025-03-28 17:55:57 -0300

    incluindo documentacao openapi

--- src/main/kotlin/com/guigon/api_cartoes/interfaceadapters/controller/CartaoController.kt
+++ src/main/kotlin/com/guigon/api_cartoes/interfaceadapters/controller/CartaoController.kt
@@ -4,6 +4,11 @@ import com.guigon.api_cartoes.application.ports.SolicitarCartaoUseCase
 import com.guigon.api_cartoes.infrastructure.config.IdadeProperties
 import com.guigon.api_cartoes.interfaceadapters.dto.request.SolicitacaoRequest
 import com.guigon.api_cartoes.interfaceadapters.dto.response.SolicitacaoResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,6 +21,14 @@ class CartaoController(
     private val solicitacaoCartaoUseCase: SolicitarCartaoUseCase,
     private val idadeProperties: IdadeProperties
 ) {
+    @Operation(summary = "Solicitar cartão")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Cartão com status aprovado",
+            content = [Content(mediaType = "application/json", schema = Schema(implementation = SolicitacaoResponse::class))]),
+        ApiResponse(responseCode = "204", description = "Cliente não possui nenhum cartã aprovado", content = [Content()]),
+        ApiResponse(responseCode = "400", description = "Requisição inválida", content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]),
+        ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))])
+    ])
     @PostMapping
     suspend fun solicitarCartao(@Valid @RequestBody request: SolicitacaoRequest): SolicitacaoResponse {
         val solicitacao = solicitacaoCartaoUseCase.solicitar(request.toSolicitacao(idadeProperties))
