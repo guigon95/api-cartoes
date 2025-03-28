0e9bc0cfd52dec2ad1edd61e8e7395e0249bdcb2
guigon95 <guilherme_gon@live.com>
2025-03-28 14:46:31 -0300

    ajustando casas decimais para valores

--- src/main/kotlin/com/guigon/api_cartoes/domain/TipoCartaoEnum.kt
+++ src/main/kotlin/com/guigon/api_cartoes/domain/TipoCartaoEnum.kt
@@ -3,9 +3,9 @@ package com.guigon.api_cartoes.domain
 import java.math.BigDecimal
 
 enum class TipoCartaoEnum(private val valorAnuidadeMensal: BigDecimal, private val valorLimiteDisponivel: BigDecimal) {
-    CARTAO_SEM_ANUIDADE(BigDecimal(0.00), BigDecimal(1000.00)),
-    CARTAO_COM_CASHBACK(BigDecimal(30.00), BigDecimal(5000.00)),
-    CARTAO_DE_PARCEIROS(BigDecimal(20.00), BigDecimal(3000.00));
+    CARTAO_SEM_ANUIDADE(BigDecimal("0.00"), BigDecimal("1000.00")),
+    CARTAO_COM_CASHBACK(BigDecimal("30.00"), BigDecimal("5000.00")),
+    CARTAO_DE_PARCEIROS(BigDecimal("20.00"), BigDecimal("3000.00"));
 
     fun criarCartao(): Cartao {
         return Cartao(this, valorAnuidadeMensal, valorLimiteDisponivel, Status.APROVADO)
--- src/main/kotlin/com/guigon/api_cartoes/interfaceadapters/dto/CartaoDto.kt
+++ src/main/kotlin/com/guigon/api_cartoes/interfaceadapters/dto/CartaoDto.kt
@@ -1,5 +1,6 @@
 package com.guigon.api_cartoes.interfaceadapters.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.guigon.api_cartoes.domain.Cartao
 import java.math.BigDecimal
@@ -8,8 +9,10 @@ data class CartaoDto(
     @JsonProperty("tipo_cartao")
     val tipoCartao: String,
     @JsonProperty("valor_anuidade_mensal")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "#0.00")
     val valorAnuidadeMensal: BigDecimal,
     @JsonProperty("valor_limite_disponivel")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "#0.00")
     val valorLimiteDisponivel: BigDecimal,
     @JsonProperty("status")
     val status: String
