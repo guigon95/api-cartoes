c09a119379344bf675f8e3c2e460a5691e800055
Guilherme <guilherme_gon@live.com>
2025-03-29 13:19:13 -0300

    melhorando circuit breaker

--- src/main/kotlin/com/guigon/api_cartoes/infrastructure/client/ClienteApiImp.kt
+++ src/main/kotlin/com/guigon/api_cartoes/infrastructure/client/ClienteApiImp.kt
@@ -2,7 +2,9 @@ package com.guigon.api_cartoes.infrastructure.client
 
 import com.guigon.api_cartoes.application.ports.ClienteApi
 import com.guigon.api_cartoes.domain.ClienteApiResponse
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.circuitbreaker.event.CircuitBreakerEvent
+import io.github.resilience4j.kotlin.circuitbreaker.executeSuspendFunction
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
 import kotlinx.coroutines.reactive.awaitSingle
@@ -16,30 +18,35 @@ import java.time.Duration
 open class ClienteApiImp(
     private val webClient: WebClient,
     private val apiUrl: String,
-    private val meterRegistry: MeterRegistry
+    private val meterRegistry: MeterRegistry,
+    private val circuitBreaker: CircuitBreaker
 ) : ClienteApi {
     private val logger = LoggerFactory.getLogger(ClienteApiImp::class.java)
 
-    @CircuitBreaker(name = "clienteapicircuitcreaker", fallbackMethod = "requisicaoExternaFallback")
     override suspend fun requisicaoExterna(): ClienteApiResponse {
         logger.info("Iniciando requisição externa para a API: $apiUrl")
         meterRegistry.counter("api.clientes.chamadas.counter").increment()
         val timer = Timer.start(meterRegistry)
         val result: ClienteApiResponse
         try {
-            result = webClient.get()
-                .uri(apiUrl).accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError) {
-                    it.toEntity<String>().map { error ->
-                        throw RuntimeException("Erro ao buscar dados externos: ${it.statusCode()} - $error")
+            result = circuitBreaker.executeSuspendFunction {
+                webClient.get()
+                    .uri(apiUrl).accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::isError) {
+                        it.toEntity<String>().map { error ->
+                            throw RuntimeException("Erro ao buscar dados externos: ${it.statusCode()} - $error")
+                        }
                     }
-                }
-                .bodyToMono(ClienteApiResponse::class.java)
-                .timeout(Duration.ofSeconds(1))
-                .awaitSingle()
+                    .bodyToMono(ClienteApiResponse::class.java)
+                    .timeout(Duration.ofSeconds(1))
+                    .awaitSingle()
+            }
         }
         finally {
+            circuitBreaker.eventPublisher.onEvent { event: CircuitBreakerEvent ->
+                logger.info("CircuitBreaker [{}] mudou de estado: {}", circuitBreaker.name, event.eventType)
+            }
             timer.stop(meterRegistry.timer("api.clientes.chamadas.timer"))
         }
         return result
