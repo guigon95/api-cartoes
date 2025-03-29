1653d935807ce9778e64d0cc8996f326df955f8e
guigon95 <guilherme_gon@live.com>
2025-03-28 22:59:27 -0300

    incluindo circuit breaker

--- src/main/kotlin/com/guigon/api_cartoes/infrastructure/client/ClienteApiImp.kt
+++ src/main/kotlin/com/guigon/api_cartoes/infrastructure/client/ClienteApiImp.kt
@@ -2,8 +2,7 @@ package com.guigon.api_cartoes.infrastructure.client
 
 import com.guigon.api_cartoes.application.ports.ClienteApi
 import com.guigon.api_cartoes.domain.ClienteApiResponse
-import com.guigon.api_cartoes.domain.exceptions.ClienteApiException
-import io.micrometer.core.annotation.Timed
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
 import kotlinx.coroutines.reactive.awaitSingle
@@ -12,16 +11,16 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.toEntity
-import reactor.util.retry.Retry
 import java.time.Duration
 
-class ClienteApiImp(
+open class ClienteApiImp(
     private val webClient: WebClient,
     private val apiUrl: String,
     private val meterRegistry: MeterRegistry
 ) : ClienteApi {
     private val logger = LoggerFactory.getLogger(ClienteApiImp::class.java)
 
+    @CircuitBreaker(name = "clienteapicircuitcreaker", fallbackMethod = "requisicaoExternaFallback")
     override suspend fun requisicaoExterna(): ClienteApiResponse {
         logger.info("Iniciando requisição externa para a API: $apiUrl")
         meterRegistry.counter("api.clientes.chamadas.counter").increment()
@@ -33,16 +32,21 @@ class ClienteApiImp(
                 .retrieve()
                 .onStatus(HttpStatusCode::isError) {
                     it.toEntity<String>().map { error ->
-                        throw ClienteApiException("Erro ao buscar dados externos: ${it.statusCode()} - $error")
+                        throw RuntimeException("Erro ao buscar dados externos: ${it.statusCode()} - $error")
                     }
                 }
                 .bodyToMono(ClienteApiResponse::class.java)
                 .timeout(Duration.ofSeconds(1))
-                .retry(3)
                 .awaitSingle()
-        } finally {
+        }
+        finally {
             timer.stop(meterRegistry.timer("api.clientes.chamadas.timer"))
         }
         return result
+    }
+
+    open fun requisicaoExternaFallback(throwable: Throwable): ClienteApiResponse {
+        logger.error("Erro ao buscar dados externos: ${throwable.message}")
+        throw RuntimeException("Erro ao buscar dados externos: ${throwable.message}")
     }
 }
